4117f0856809c2fb23c714c23dcd2446d588cc63
ChengQing <darkblue035@gmail.com>
2023-01-10 16:18:14 +0800

    Add support for Keychron V2 knob

---
+++ keyboards/keychron/v2/v2_ansi_stm32l432_ec11/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x63, 0x24, 0x91, 0x79, 0x23, 0xD0, 0xC2, 0x73}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
